f14a7fca66a5e54fce78cf857217dad7a5247125
Hamid_test <hamid.tanhaei@gmail.com>
2020-10-18 04:17:18 +0330

    updated

---
+++ DSP_Workspace/.metadata/.plugins/org.eclipse.core.resources/.history/9/f0f8dcfa86ad001a1f5986698f886694
@@ -0,0 +1,15 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+main.obj: ../main.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.6.LTS/bin/cl2000" -v28 -ml -mt --cla_support=cla0 -O3 --opt_for_speed=2 --include_path="D:/GitHub_MyRep/DSP_PWM_FullBridge/DSP_Workspace/DSP_PWM_FullBridge" --include_path="D:/GitHub_MyRep/DSP_PWM_FullBridge/DSP_Workspace/DSP_PWM_FullBridge/include" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.6.LTS/include" --advice:performance=all -g --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="main.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
---
+++ DSP_Workspace/DSP_PWM_FullBridge/Release/main.d
@@ -0,0 +1,71 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP28x_Project.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Cla_typedefs.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Device.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Adc.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_BootVars.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_DevEmu.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Cla.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Comp.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_CpuTimers.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_ECan.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_ECap.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_EPwm.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_EQep.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Gpio.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_HRCap.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_I2c.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Lin.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_NmiIntrupt.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_PieCtrl.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_PieVect.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Spi.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Sci.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_SysCtrl.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_XIntrupt.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Examples.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_GlobalPrototypes.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_EPwm_defines.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_I2c_defines.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Cla_defines.h
+main.obj: G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_DefaultISR.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.6.LTS/include/stdio.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.6.LTS/include/linkage.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.6.LTS/include/stdarg.h
+
+../main.c: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP28x_Project.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Cla_typedefs.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Device.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Adc.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_BootVars.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_DevEmu.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Cla.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Comp.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_CpuTimers.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_ECan.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_ECap.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_EPwm.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_EQep.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Gpio.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_HRCap.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_I2c.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Lin.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_NmiIntrupt.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_PieCtrl.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_PieVect.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Spi.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Sci.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_SysCtrl.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_XIntrupt.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Examples.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_GlobalPrototypes.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_EPwm_defines.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_I2c_defines.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_Cla_defines.h: 
+G:/DSP/WorkSpace/AM_RxTx_V5_0/include/DSP2803x_DefaultISR.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.6.LTS/include/stdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.6.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.6.LTS/include/stdarg.h: 
